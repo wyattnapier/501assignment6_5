8265ded5e76a6e126d7b2611c61804ba7bd36b22
Wyatt <wnapier@bu.edu>
2025-11-23 20:04:08 -0500

    add current location to the map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
     implementation(libs.androidx.material3)
     implementation(libs.maps.compose)
     testImplementation(libs.junit)
--- app/src/main/java/com/example/assign6_5/MainActivity.kt
+++ app/src/main/java/com/example/assign6_5/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.assign6_5
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -10,12 +20,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.google.maps.android.compose.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -28,35 +41,126 @@ import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
+import com.google.android.gms.location.*
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MapDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let { location ->
+                    setContent {
+                        MapDemoTheme {
+                            // A surface container using the 'background' color from the theme
+                            Surface(
+                                modifier = Modifier.fillMaxSize(),
+                                color = MaterialTheme.colorScheme.background
+                            ) {
+                                MainScreen(location)
+                            }
+                        }
+                    }
                 }
+            }
+        }
+
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    com.example.assign6_5.RequestPermissions(
+                        fusedLocationClient,
+                        locationRequest,
+                        locationCallback
+                    )
+                }
+            }
+        }
+
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    override fun onResume() {
+        super.onResume()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+}
+
+@Composable
+fun RequestPermissions(
+    fusedLocationClient: FusedLocationProviderClient,
+    locationRequest: LocationRequest,
+    locationCallback: LocationCallback
+) {
+    val context = LocalContext.current
+    var hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        hasLocationPermission = isGranted
+        if (isGranted) {
+            try {
+                fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+            } catch (e: SecurityException) {
+                Log.e("Location", "Security Exception: ${e.message}")
+            }
+        }
+    }
+
+    LaunchedEffect(hasLocationPermission) {
+        if (!hasLocationPermission) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            try {
+                fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+            } catch (e: SecurityException) {
+                Log.e("Location", "Security Exception: ${e.message}")
             }
         }
     }
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(location: Location) {
 
-    val marina = LatLng(33.875771, -78.001839)
+    val target = LatLng(location.latitude, location.longitude)
     val cameraPositionState = rememberCameraPositionState {
-        // position = CameraPosition.fromLatLngZoom(marina, 18f)
         position = CameraPosition.Builder()
-            .target(marina)
-            .zoom(25f)
-            .bearing(70f)
-            .tilt(80f)
+            .target(target)
+            .zoom(15f)
+            .bearing(0f)
+            .tilt(0f)
             .build()
     }
 
@@ -75,7 +179,7 @@ fun MainScreen() {
         uiSettings = uiSettings
     ) {
         Marker(
-            state = MarkerState(position = marina),
+            state = MarkerState(position = target),
             title = "Marina",
             snippet = "Bald Head Island Marina"
         )
@@ -86,7 +190,12 @@ fun MainScreen() {
 @Preview(showBackground = true)
 @Composable
 fun MapPreview() {
+    val testLocation: Location = Location("").apply {
+        latitude = 42.3555
+        longitude = -71.0565
+    }
+
     MapDemoTheme {
-        MainScreen()
+        MainScreen(testLocation)
     }
 }
